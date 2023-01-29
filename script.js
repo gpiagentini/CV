77bbcb1fdef431163caebdedf6711f5127c29812
Gabriel Piagentini <gmtpiagentini@gmail.com>
2023-01-29 01:18:26 -0300

    Added 'About me' section

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const BIRTHDAY = new Date("1999-02-02")
+
+document.getElementById("age").innerText = calculateAge()
+
 function openNewTab(url) {
     window.open(url, '_blank')
+}
+
+function calculateAge() {
+    var yearDiff = new Date(Date.now() - new Date(BIRTHDAY)).getFullYear()
+    return yearDiff - 1970
 }
